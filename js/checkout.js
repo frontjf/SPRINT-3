737e2595c15d98461f51d44b75c68797e81481ce
frontjf <frontjf@gmail.com>
2023-03-29 13:18:50 +0200

    validacion ok he puesto alerts

--- js/checkout.js
+++ js/checkout.js
@@ -3,91 +3,91 @@
 
 
  // Exercise 6
-    function validate() {
+
+ function validate() {
 		
-    	let error = 0;
-    	// Get the input fields
-    	let fName = document.getElementById("fName");
-    	let fEmail = document.getElementById("fEmail");
-		let fLastN = document.getElementById("fLastN").value;
-		let fAddress = document.getElementById("fAddress").value;
-		let fPassword = document.getElementById("fPassword").value;
-		let fPhone = document.getElementById("fPhone").value;		
+	let error = 0;
+	// Get the input fields
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+	let fLastN = document.getElementById("fLastN");
+	let fAddress = document.getElementById("fAddress");
+	let fPassword = document.getElementById("fPassword");
+	let fPhone = document.getElementById("fPhone");		
 
 
 
-      let regex = /^[a-zA-Z]{3,}$/;                                       // Expresión regular que solo permite letras y un mínimo de 3 caracteres
-      let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    // Expresión regular para validar correo electrónico
-      let regexPass = /^[a-zA-Z0-9]+$/g;						          // Expresión regular para validar contraseña
-      let regexPhone = /^[0-9]{0,15}$/;                                   // Expresión regular para validar teléfono
+  let regex = /^[a-zA-Z]{3,}$/;                                       // Expresión regular que solo permite letras y un mínimo de 3 caracteres
+  let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    // Expresión regular para validar correo electrónico
+  let regexPass = /^[a-zA-Z0-9]+$/g;						          // Expresión regular para validar contraseña
+  let regexPhone = /^[0-9]{0,15}$/;                                   // Expresión regular para validar teléfono
+
+
+	// Get the error elements
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");
+	let errorLastN = document.getElementById("errorLastN");
+	let errorAddress = document.getElementById("errorAddress");
+	let errorPassword = document.getElementById("errorPassword");
+	let errorPhone = document.getElementById("errorPhone");
+
 	
 
-    	// Get the error elements
-    	let errorName = document.getElementById("errorName");
-    	let errorEmail = document.getElementById("errorEmail");
-		let errorLastN = document.getElementById("errorLastN");
-		let errorAddress = document.getElementById("errorAddress");
-		let errorPassword = document.getElementById("errorPassword");
-		let errorPhone = document.getElementById("errorPhone");
 
+	// Validate fields entered by the user: name, phone, password, and email
+	if(fName.value == "" || !regex.test(fName.value)){
 		
-
+		fName.classList.add('border-danger');   				// con esta linia me sale uncaugh en la consola de forma rapida y desaparece
+		//errorName.className.add('is-invalid');					// mismo error que arriba
+		alert('nombre');
+		//fName.style.border = '1px solid red';
+		error++;
+		
+	}
 	
-    	// Validate fields entered by the user: name, phone, password, and email
-    	if(fName.value == "" || !regex.test(fName.value)){
-			
-			//fName.classList.add('border border-danger is-invalid');   // con esta linia me sale uncaugh en la consola de forma rapida y desaparece
-			//errorName.className.add('is-invalid');					// mismo error que arriba
-    		
-			error++;
-			
-    	}
+  
+	if(fEmail.value == "" || !regexMail.test(fEmail.value)){
+		alert('mail');
+		error++;
+	}
+
+	if(fLastN.value == "" || !regex.test(fLastN.value)){
+		alert('apellido')
+		error++;
+
+
+	}
+
+	if(fAddress.value == "" || !regex.test(fAddress.value)){
+		alert('direccion');
+		error++;
+
+	}
+
+	if(fPassword.value == "" || !regexPass.test(fPassword.value)){
+		alert('error pass');
+		error++;
+
 		
-      
-    	if(fEmail.value == "" || !regexMail.test(fEmail.value)){
-        
-    		error++;
-    	}
+	 }
 
-		if(fLastN.value == "" || !regex.test(fLastN.value)){
+	if(fPhone.value == "" || !regexPhone.test(fPhone.value)){
+		alert('telefono erroneo');
+		error++;
 
-			error++;
+		
+	}
+   
+	 if(error>0){
+		 alert("Error");
+	 }else{
+		 alert("OK");
+	 }
+		
 
-
-		}
-
-		if(fAddress.value == "" || !regex.test(fAddress.value)){
-
-			error++;
-
-		}
-
-		if(fPassword.value == "" || !regexPass.test(fPassword.value)){
-
-		 	error++;
-
-			
-		 }
-
-		if(fPhone.value == "" || !regexPhone.test(fPhone.value)){
-
-			
-		}
-       
-    	 if(error>0){
-    	 	alert("Error");
-    	 }else{
-    	 	alert("OK");
-    	 }
-			
-
-    }
-
-
-
+}
+    
  
-
-
 
 // function validate(){
 
