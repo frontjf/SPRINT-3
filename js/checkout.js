a5604054a195e151315d8d23b44254a1fa9374e6
frontjf <frontjf@gmail.com>
2023-04-13 00:03:53 +0200

    validacion

--- js/checkout.js
+++ js/checkout.js
@@ -1,8 +1,11 @@
-//   onsubit
-
-
-
  // Exercise 6
+	
+	 
+
+function validate() {
+
+	
+
 	let form = document.getElementById('form');
  
  	let fName = document.getElementById("fName");
@@ -19,7 +22,9 @@
   	let regexPhone = /^[0-9]{0,15}$/;                                   // Expresión regular para validar teléfono
 
 
-function validate() {
+
+
+	
 	
 		
 	let error = 0;
@@ -34,7 +39,6 @@ function validate() {
 	let errorPassword = document.getElementById("errorPassword");
 	let errorPhone = document.getElementById("errorPhone");
 
-	
 
 
 	// Validate fields entered by the user: name, phone, password, and email
@@ -42,255 +46,82 @@ function validate() {
 		
 		//fName.classList.add('border-danger');   				// con esta linia me sale uncaugh en la consola de forma rapida y desaparece
 		
-		//alert('error nombre');
-		errorName.classList.add('is-invalid');				
-		errorName.style.display = 'block'; 
-		fName.style.border = '1px solid red';
+		
+		errorName.classList.add('is-invalid');
+		errorName.style.display = 'block';
+					
 		error++;
 		
+	}
+	else {
+		errorName.style.display = 'none';
 	}
 	
   
 	if(fEmail.value == "" || !regexMail.test(fEmail.value)){
-		//alert('error mail');
-		fEmail.style.border = '1px solid red';
-		errorEmail.classList.add('is-invalid');				
-		errorEmail.style.display = 'block'; 
+		
+		errorEmail.classList.add('is-invalid');
+		errorEmail.style.display = 'block';
 		error++;
+	}
+	else {
+		errorEmail.style.display = 'none';
 	}
 
 	if(fLastN.value == "" || !regex.test(fLastN.value)){
-		//alert('error apellido')
-		errorLastN.classList.add('is-invalid');				
+		
+		errorLastN.classList.add('is-invalid');
 		errorLastN.style.display = 'block'; 
-		fLastN.style.border = '1px solid red';
 		error++;
-
-
+	}
+	else {
+		errorLastN.style.display = 'none';
 	}
 
 	if(fAddress.value == "" || !regex.test(fAddress.value)){
-		//alert('error direccion');
-		errorAddress.classList.add('is-invalid');				
-		errorAddress.style.display = 'block'; 
-		fAddress.style.border = '1px solid red';
+		
+		errorAddress.classList.add('is-invalid');
+		errorAddress.style.display = 'block';				
 		error++;
 
+	}
+	else {
+		errorAddress.style.display = 'none';
 	}
 
 	if(fPassword.value == "" || !regexPass.test(fPassword.value)){
-		//alert('error pass');
-		errorPassword.classList.add('is-invalid');				
-		errorPassword.style.display = 'block'; 
-		fPassword.style.border = '1px solid red';
-		error++;
-
 		
-	 }
+		errorPassword.classList.add('is-invalid');
+		errorPassword.style.display = 'block';				
+		error++;
+	}
+	else {
+		errorPassword.style.display = 'none';
+	}
 
 	if(fPhone.value == "" || !regexPhone.test(fPhone.value)){
-		//alert('telefono erroneo');
-		errorPhone.classList.add('is-invalid');				
-		errorPhone.style.display = 'block'; 
-		fPhone.style.border = '1px solid red';
-		error++;
-
 		
+		errorPhone.classList.add('is-invalid');
+		errorPhone.style.display = 'block';
+		error++;
+	}
+	else {
+		errorPhone.style.display = 'none';
 	}
    
-	 if(error>0){
-		
-		//alert("Error");
-		
-				
-	 }else{
-		 alert("OK");
-	 }
-		
-	 
+	if(error>0){
+		return false;
+	}
+	else{
+		alert("OK");
+		return true;
+	}
+	
 
 }
-//document.addEventListener('click', validate);
+/*
 form.addEventListener("submit", (e) =>{
 	e.preventDefault();
 	validate();
 });
-
-
-
-
-    
- 
-
-// function validate(){
-
-        
-//          validateName();
-//          validateSurName();
-//          validateAdress();
-//          validatePass();
-//          validatePhone();
-//          validateMail();
-//    }
-
-
-
-// function validateName(){  
-
-//          let fName = document.getElementById("fName").value;
-//         // let regex = /^[a-zA-Z]{3,}$/;                                   // Expresión regular que solo permite letras y un mínimo de 3 caracteres
-//          let errorName = document.getElementById("errorName");           // Obtener el elemento para el mensaje de error
-      
-
-//          if (!fName.value.match(/^[A-Za-z\s]*$/) || fName.value.length < 3) {
-//             fName.classList.add("is-invalid");
-//             //errorName.style.display = "block";
-//         } else {
-//             fName.classList.remove("is-invalid");
-//             errorName.style.display = "none";
-//         }
-        
-
-
-
-         // if (!regex.test(fName)) {
-         //   errorName.innerHTML = "El nombre debe contener solo letras y tener un mínimo de 3 caracteres.";       // Agregar el mensaje de error
-         //   document.getElementById("fName").style.border = "1px solid red";                                  // Cambiar el borde a rojo
-         //   return false;
-         // } else {
-         //   errorName.innerHTML = "";                                                     // Restablecer el mensaje de error
-         //   document.getElementById("fName").style.border = "none";                   // Restablecer el borde a su estado original
-         //   return true;
-         // }
-//      }
-
-// function validateSurName(){
-
-//          let fLastN = document.getElementById("fLastN").value;
-//          let regex = /^[a-zA-Z]{3,}$/;                                   
-//          let errorLastN = document.getElementById("errorLastN");           
-      
-//          if (!regex.test(fLastN)) {
-//            errorLastN.innerHTML = "El nombre debe contener solo letras y tener un mínimo de 3 caracteres.";       
-//            document.getElementById("fLastN").style.border = "1px solid red";                                  
-//            return false;
-//          } else {
-//            errorLastN.innerHTML = "";                                                     
-//            document.getElementById("fLastN").style.border = "none";                   
-//            return true;
-//          }
-//      }
-
-// function validateAdress(){
-
-//          let fAddress = document.getElementById("fAddress").value;
-//          let regex = /^[a-zA-Z]{3,}$/;                                   
-//          let errorAddress = document.getElementById("errorAddress");           
-     
-//          if (!regex.test(fAddress)) {
-//            errorAddress.innerHTML = "El nombre debe contener solo letras y tener un mínimo de 3 caracteres.";       
-//            document.getElementById("fAddress").style.border = "1px solid red";                                  
-//            return false;
-//          } else {
-//            errorAddress.innerHTML = "";                                                     
-//            document.getElementById("fAddress").style.border = "none";                   
-//            return true;
-//          }
-
-//      }
-
-// function validateMail(){
-
-
-//          let fEmail = document.getElementById("fEmail").value;
-//          let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // Expresión regular para validar correo electrónico
-//          let errorEmail = document.getElementById("errorEmail"); // Obtener el elemento para el mensaje de error
-
-//          if (!regex.test(fEmail)) {
-//              errorEmail.innerHTML = "Introduzca una dirección de correo electrónico válida"; // Agregar el mensaje de error
-//              document.getElementById("fEmail").style.border = "1px solid red"; // Cambiar el borde a rojo
-//              return false;
-//          } else {
-//                  errorEmail.innerHTML = ""; // Restablecer el mensaje de error
-//                  document.getElementById("fEmail").style.border = "none"; // Restablecer el borde a su estado original
-//                  return true;
-//              }
-
-//      }
-
-// function validatePass(){
-
-        
-//              let fPassword = document.getElementById("fPassword").value;
-//              let regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;               // Expresión regular para validar contraseña
-//              let errorPassword = document.getElementById("errorPassword");       // Obtener el elemento para el mensaje de error
-         
-//              if (!regex.test(fPassword)) {
-//                errorPassword.innerHTML = "La contraseña debe incluir al menos una letra y un número, y tener una longitud de al menos 8 caracteres"; // Agregar el mensaje de error
-//                document.getElementById("fPassword").style.border = "1px solid red";          // Cambiar el borde a rojo
-//                return false;
-//              } else {
-//                errorPassword.innerHTML = "";                                             // Restablecer el mensaje de error
-//                document.getElementById("fPassword").style.border = "none";           // Restablecer el borde a su estado original
-//                return true;
-//              }
-        
-          
-
-//      }
-
-// function validatePhone(){
-
-//              let fPhone = document.getElementById("fPhone").value;
-//              let regex = /^[0-9]{0,15}$/;                                      // Expresión regular para validar teléfono
-//              let errorPhone = document.getElementById("errorPhone");         // Obtener el elemento para el mensaje de error
-
-//                  if (!regex.test(fPhone)) {
-//                      errorPhone.innerHTML = "El teléfono debe contener solo números y tener una longitud máxima de 9 caracteres"; // Agregar el mensaje de error
-//                      document.getElementById("fPhone").style.border = "1px solid red"; // Cambiar el borde a rojo
-//                  return false;
-//                  } else {
-//                      errorPhone.innerHTML = "";                                          // Restablecer el mensaje de error
-//                        document.getElementById("fPhone").style.border = "none";        // Restablecer el borde a su estado original
-//                      return true;
-//                  }
-//      }
-
-
- 
-        
-// function validate() {
-//   var error = 0;
-//   // Get the input fields
-//   var fName = document.getElementById("fName");
-//   var fEmail = document.getElementById("fEmail");
-
-
-//   // Get the error elements
-//   var errorName = document.getElementById("errorName");
-//   var errorEmail = document.getElementById("errorEmail");  
-
-//   // Validate fields entered by the user: name, phone, password, and email
-//   if(fName.value == ""){
-//     error++;
-//   }
-
-//   if(fEmail.value == ""){
-//     error++;
-//   }
-
-//   if(error>0){
-//     alert("Error");
-//   }else{
-//     alert("OK");
-//   }
-
-// }        
-    
-      
-        
-    
-        
-
-  
-
+*/
--- js/shop.js
+++ js/shop.js
@@ -1,240 +1,243 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
+let products = [
     {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
+         id: 1,
+         name: 'cooking oil',
+         price: 10.5,
+         type: 'grocery',
+         offer: {
+             number: 3,
+             percent: 20
+         }
+     },
+     {
+         id: 2,
+         name: 'Pasta',
+         price: 6.25,
+         type: 'grocery'
+     },
+     {
+         id: 3,
+         name: 'Instant cupcake mixture',
+         price: 5,
+         type: 'grocery',
+         offer: {
+             number: 10,
+             percent: 30
+         }
+     },
+     {
+         id: 4,
+         name: 'All-in-one',
+         price: 260,
+         type: 'beauty'
+     },
+     {
+         id: 5,
+         name: 'Zero Make-up Kit',
+         price: 20.5,
+         type: 'beauty'
+     },
+     {
+         id: 6,
+         name: 'Lip Tints',
+         price: 12.75,
+         type: 'beauty'
+     },
+     {
+         id: 7,
+         name: 'Lawn Dress',
+         price: 15,
+         type: 'clothes'
+     },
+     {
+         id: 8,
+         name: 'Lawn-Chiffon Combo',
+         price: 19.99,
+         type: 'clothes'
+     },
+     {
+         id: 9,
+         name: 'Toddler Frock',
+         price: 9.99,
+         type: 'clothes'
+     }
 ]
-// Array with products (objects) added directly with push(). Products in this array are repeated.
-var cartList = [];
 
-// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
-   
+let cartList = [];
 
-var total = 0;
+let cart = [];
 
-// Exercise 1
+let total = 0;
+
+
+// Ejercicio 1
 function buy(id) {
 
-   for (let i =0; i<products.length; i++){     // recorre array products
+    for (let i =0; i<products.length; i++){     // recorre array products
 
         if (products[i].id === id){            // si la id que pasa la funcion buy coincide con la id del producto lo añade a cartList
             cartList.push(products[i]);        
         }
         
    }
+
+   generateCart();
+   document.getElementById("count_product").innerHTML = cartList.length;   // pone el numero de productos en el HTML 'count_product'
    
-   console.log(cartList);
+   
 
-    // let productOk = products.find(prod=>id === prod.id);
-    // cartList.push(productOk);
-
-
-    document.getElementById("count_product").innerHTML = cartList.length;   // pone el numero de productos en el HTML 'count_product'
-    calculateTotal();         // llama a la funcion que calcula el total
-    generateCart();           // llama a la funcion que suma solo la cantidad si el producto ya esta en la cesta de la compra y asi no lo repita  
-    
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
 }
 
-// Exercise 2
+// Ejercicio 2
 function cleanCart() {
 
-    cartList.length = 0;
-    document.getElementById("count_product").innerHTML = cartList.length
+    
+    cartList.length = 0;                            // ponemos cartList a 0
+    printCart(cart);                                // como cartList esta vacia esta funcion no dibuja nada en html pq no hay productos
+    calculateTotal(cart);                           // al no haber producto el calculo final es 0
+    document.getElementById("count_product").innerHTML = cartList.length        // aparecera un 0 en el icono del carrito
+    document.getElementById('total_price').innerHTML = '';                      // y el precio total tambien vacio
+    
+
     console.log(cartList);
+    
+
 
 }
 
-// Exercise 3
+// Ejercico 3 
 
-function calculateTotal() {
+function calculateTotal(cart) {
+
+    total = 0;
+    for (let i = 0; i < cart.length; i++) {             // recorremos cart sumando el precio con el dto
+          total += cart[i].subtotalWithDiscount;
+        }
     
+    return total;
     
+
+    
+  
+}
+
+
+
+
+// Ejercicio 4
+
+
+function generateCart() {
+    // Crear el array 'cart' a partir de 'cartList'
+    var cart = cartList.map(function(item) {
+      var newItem = {};
+      newItem.name = item.name;
+      newItem.type = item.type;
+      newItem.price = item.price;
+      newItem.offer = item.offer;
+      newItem.quantity = 1;
+      newItem.subtotal = item.price;
+      newItem.subtotalWithDiscount = item.price;
+      return newItem;
+    });
+  
+    // Actualizar la cantidad y los subtotales si se agregan elementos repetidos
+
+     for (let i = 0; i < cart.length; i++) {                    // bucle dentro de otro que revisa si hay items repetidos
+       for (let j = i + 1; j < cart.length; j++) {
+         if (cart[i].name === cart[j].name) {                   // aqui mira si existe uno igual
+           cart[i].quantity += 1;                               // si lo hay, aumenta la cantidad
+           cart[i].subtotal += cart[j].price;                   // le pone el precio (o lo suma si ya hubiera uno anterior)
+           cart[i].subtotalWithDiscount += cart[j].price;       // aqui lo mismo con el subTotal, lo pone o lo suma si ya hay uno
+           cart.splice(j, 1);                                   // elimina el elemento repetido
+           j--;                                                 // decrementa el contador, al borrarse se mueven a la derecha y si no se decrementa 
+         }                                                      // omitira el siguiente elemento    
+       }
+     }
+  
+    applyPromotionsCart(cart);
+    calculateTotal(cart);
+    printCart(cart);
+    
+    console.log(total);                                  
+    console.log(cart);
+  }
+  
+  // Ejercicio 5
+
+
+function applyPromotionsCart(cart){
+
     for (let i=0; i < cart.length; i++){
 
-        total += cart[i].price              // recorre array cartList y va sumando el precio
-        
-    }
-    console.log(total);
-    // applyPromotionsCart();
-    // Calculate total price of the cart using the "cartList" array
-}
-
-
-
-
-// Exercise 4
-function generateCart() {
-
-    let cart1 = [];
-    
-    
-     for (let i =0; i<cartList.length; i++){
-        let actualProduct = cartList[i];                    // meto cartList en una variable actualProduct
-                            
-        let istheProduct = cart1.includes(actualProduct);    // aqui pongo los productos de cart que estan incluidos en actualProduct
-
-        if (istheProduct){                                   // si el producto esta incluido se aumenta la cantidad
-                                             
-            actualProduct.quantity++
+        if (cart[i].name === 'cooking oil' && cart[i].quantity >=3){                    // si se llama cooking oil y hay mas de 3 unidades
+            cart[i].subtotalWithDiscount = cart[i].quantity *10                         // cada unidad valdra 10eu 
         }
-        else {                                              // si no esta incluido el producto, añado ese producto al array cart con el metodo push
-            
-            actualProduct.quantity = 1
-            cart1.push(actualProduct)
-            
-        }
-        
-          
+        else if(cart[i].name === 'Instant cupcake mixture' && cart[i].quantity >=10){       // si es Instant cupcake y hay mas de 10 unidades
+            //cart[i].subtotalWithDiscount = (cart[i].price*2/3)*cart[i].quantity;          
+            cart[i].subtotalWithDiscount = (cart[i].price * 2 * cart[i].quantity) /3;           // el precio se reduce 2/3
+            //cart[i].subtotalWithDiscount = cart[i].subtotalWithDiscount.toFixed(2);               // redondeamos a 2 decimales
+            cart[i].subtotalWithDiscount = parseFloat(cart[i].subtotalWithDiscount.toFixed(2));     // asi evitamos que se '033.00'
+                       
+        }      
     }
     
-        cart = cart1.map(function(producto) {        // metodo map se crea nuevo array, se crea una funcion dentro que quita la id y añade mas valores       
-        cart = [];
-        for (let clave in producto) {
-          if (clave !== "id") {                     // si el valor es distinto a id entonces lo añade al array cart
-            cart[clave] = producto[clave];          // la clave de cart sera el producto, o sea el nombre
-          }
-        }
-        cart.subtotal = 0;                      // se añaden 2 nuevos valores al array creada llamada cart
-        cart.subtotalWithDiscount = 0;
-        return cart;
-      });
-     
-      
-    //applyPromotionsCart();
-    console.log(cart);
     
     
+}
 
-    // recorrer el array 'cartList' y añadir items nuevos al array 'cart', si un item ya existe se ha de sumar el precio 
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+// Ejercicio 6
 
+function printCart(cart) {
+
+    document.getElementById('total_price').innerHTML = total;      // ponemos precio total
+    let table = document.getElementById('cart_list');              // en la variable table ponemos el html donde escribiremos
+    let listaCompra = [];                                          // array para poner productos seleccionados de cart 
     
+
+
+    for(i=0; i<cart.length;i++){                                    // recorremos cart
+        if (cart[i].subtotalWithDiscount){                          // si tiene el dto (quiere decir que el producto esta seleccionado)
+        let fila = `<tr>                                            
+
+        <td>${cart[i].name}</td>
+        <td>${cart[i].price}</td>
+        <td>${cart[i].quantity}</td>
+        <td>${cart[i].subtotalWithDiscount}</td>
+        </tr>`
+        listaCompra.push(fila);
+        }                                   // en la variable fila escribimos la tabla y ponemos los productos con el $ de javascript correspondiente
+                                            // al item del producto que hay seleccionado en cart
+    
+    } 
+    
+    table.innerHTML = listaCompra.join('');  // añadimos '' a la tabla
+
+
 }
 
-// Exercise 5
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
-
-   
-
-    for (let i=0; i>cart.length; i++){
-        
-         if (cart[i].name == 'cooking oil' && cart[i].quantity >= 3){
-                          
-             cart[i].subtotalWithDiscount = cart[i].quantity * 10;
-             
-        }
-       
-    }
-
-            
-}
-
-// function applyPromotionsCart() {
-//     // Apply promotions to each item in the array "cart"
-//     for (i = 0; i < cart.length; i++) {
-//         if (cart[i].name == 'cooking oil' && cart[i].quantity >= 3)
-//             //promoción aceite si compra más de 3 botellas             
-//             cart[i].subtotalWithDiscount = cart[i].quantity * 10;
 
 
-//         else if (cart[i].name == 'Instant cupcake mixture' && cart[i].quantity >= 10)
-//             //con más de 10 productos para hacer pastel
-//             cart[i].subtotalWithDiscount = (cart[i].subtotal / 3) * 2; 
-//         else {
-//             delete cart[i].subtotalWithDiscount;            
-//         }
-//         cart[i].subtotal = cart[i].quantity * cart[i].price;
-//     }
 
-// }
 
-// Exercise 6
-function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
-}
 
-// ** Nivell II **
-
-// Exercise 7
-function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
-}
-
-// Exercise 8
-function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
 
 function open_modal(){
+    
 	console.log("Open Modal");
-	printCart();
-    applyPromotionsCart();
+	
     
 }
+
+
+
+
+  
+      
+  
+
+
+
+
+
+
